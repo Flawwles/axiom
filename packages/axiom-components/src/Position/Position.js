ccfedb80db856bae9bd4c33f0d06f5503c50aec7
Matthew Evans <matt_-@hotmail.co.uk>
2019-10-30 10:21:26 +0000

    fix(Position): Blurry text on zoom

--- packages/axiom-components/src/Position/Position.js
+++ packages/axiom-components/src/Position/Position.js
@@ -139,6 +139,9 @@ export default class Position extends Component {
         preventOverflow: {
           boundariesElement,
         },
+        computeStyle: {
+          gpuAcceleration: false,
+        },
       },
     });
   }
